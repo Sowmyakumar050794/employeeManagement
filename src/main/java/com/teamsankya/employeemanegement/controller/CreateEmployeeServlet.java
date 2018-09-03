64539478e27842e07dbb04953c35e03639497e34
PrathibhaPrabhu <prathibhaprabhu18@gmail.com>
2018-09-03 22:38:30 +0530

    JSP HOME PAGES1&2,CRUD QUERIES,HEADER CSS,SERACHPROFILE JSP,CREATEEMPLOYEE SERVLET

--- src/main/java/com/teamsankya/employeemanegement/controller/CreateEmployeeServlet.java
+++ src/main/java/com/teamsankya/employeemanegement/controller/CreateEmployeeServlet.java
@@ -51,9 +51,8 @@ public class CreateEmployeeServlet extends HttpServlet {
 		per.setId((req.getParameter("eid")));
 		per.setEmail(req.getParameter("email"));
 		per.setCellNo(Long.parseLong(req.getParameter("mob_num")));
-		per.setDob(Date.parse(req.getParameter("DOB")));
-	
-		
+		per.setDob(Date.parse((req.getParameter("DOB"))));
+
 		EmployeeBean bean = new EmployeeBean();
 		bean.setAddr(addr);
 		bean.setBasic(info);
